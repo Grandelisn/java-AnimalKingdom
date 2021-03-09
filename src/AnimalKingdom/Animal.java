7bc381be6d978e147bee9121e0afff70aab0e753
Grandelisn <grandelisn@gmail.com>
2021-03-09 18:35:51 -0500

    mvp y'all

--- src/AnimalKingdom/Animal.java
+++ src/AnimalKingdom/Animal.java
@@ -1,5 +1,26 @@
 package AnimalKingdom;
 
 abstract class Animal {
-    
+    protected int maxId;
+    protected int id;
+    protected int energy;
+    protected String name;
+    protected int year;
+    public Animal(String name, int year){
+        maxId++;
+        energy = 1;
+        id = maxId;
+        this.name = name;
+        this.year = year;
+    }
+    public void consumeFood(int amount){
+        energy += amount;
+        System.out.println(name + " consumed " + amount + " food. Energy is now " + energy);
+
+    }
+    abstract String getMove();
+    abstract String getBreathe();
+    abstract String getReproduce();
+    abstract int getYear();
+    abstract String getName();
 }
